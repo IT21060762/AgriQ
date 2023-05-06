2272abde6873b307d772e9400d3198d01c6e377a
IT21060762 <dewmithwishmitha@gmail.com>
2023-05-06 18:35:16 +0530

    initial commit

---
+++ app/src/main/java/com/example/agriq/activity_intro.kt
@@ -0,0 +1,21 @@
+package com.example.agriq
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class activity_intro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        var continue_button = findViewById<TextView>(R.id.continue_button)
+        continue_button.setOnClickListener {
+            val intent = Intent(this, DonateActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
